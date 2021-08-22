721ddb499df081689e50cbdec4959a1671eea64c
kikaswap <kikaswap@outlook.com>
2021-08-22 17:57:07 +0800

    add IUniswapV2Factory

--- interface/IUniswapV2Factory.sol
+++ interface/IUniswapV2Factory.sol
@@ -3,4 +3,6 @@ pragma solidity >=0.6.0;
 interface IUniswapV2Factory {
     function getPair(address token0, address token1) external view returns(address);
     function createPair(address tokenA, address tokenB) external returns (address pair);
+    function allPairsLength() external view returns (uint);
+    function allPairs(uint) external view returns (address);
 }
